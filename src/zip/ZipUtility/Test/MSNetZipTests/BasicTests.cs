0a04890d25b83b045fa14f4a3260115d28db80fb
John Kosh <jkosh@envisionrx.com>
2019-09-13 10:17:34 -0400

    Updated MS Test project

--- src/zip/ZipUtility/Test/MSNetZipTests/BasicTests.cs
+++ src/zip/ZipUtility/Test/MSNetZipTests/BasicTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZipLib.Enums;
+using ZipLib.Ext;
 
 namespace MSNetZipTests
 {
@@ -11,5 +15,24 @@ namespace MSNetZipTests
     {
       Assert.IsTrue(true); /* Sanity check to make sure test framework is working */
     }
+
+    [TestMethod]
+    public void CanGzipFile_Test01_True()
+    {
+      /* Arrange */
+      var sutFilePath = Path.Combine(Environment.CurrentDirectory, TestConstants.INPUT_FILE_01_PATH);
+      var sutOutputFilePath = Path.Combine(Environment.CurrentDirectory, TestConstants.OUTPUT_FILE_01_PATH);
+      var gotInput = new FileInfo(sutFilePath);
+      var got = new FileInfo(sutOutputFilePath);
+
+      /* Act */
+      Trace.WriteLine("File already exists:{0}", got.Exists.ToString());
+      got = gotInput.GZipCompress(got, 60000);
+
+      /* Assert */
+      Trace.WriteLine("File created:{0}", got?.FullName);
+      Assert.IsTrue(got.Exists, $"{TestConstants.CANNOT_FIND_FILE_MSG} {got.FullName}");
+    }
+
   }
 }
---
+++ src/zip/ZipUtility/Test/MSNetZipTests/TestConstants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSNetZipTests
+{
+  public static class TestConstants
+  {
+    public const string CANNOT_FIND_FILE_MSG = "Cannot find the file specified:";
+
+    public const string CLEAN_UP_DIR_01 = @"elm";
+
+    /* GZip Compression */
+
+    public const string INPUT_FILE_01_PATH = @"elm\test-file-01.txt";
+
+    public const string INPUT_FILE_01_COPY_PATH = @"elm\test-file-01-copy.txt";
+
+    public const string OUTPUT_FILE_01_PATH = @"elm\test-file-01.gz";
+
+    /* GZip Decompression */
+
+    public const string CONTROL_INPUT_FILE_02_PATH = @"elm\control-file-02.gz";
+
+    public const string CONTROL_OUTPUT_FILE_02_PATH = @"elm\control-file-02.txt";
+
+    public const string INPUT_FILE_02_PATH = @"elm\test-file-02.gz";
+
+    public const string OUTPUT_FILE_02_PATH = @"elm\test-file-02.txt";
+  }
+}
