2cbdda5709145e0a86c926e3ae2760aef5d048ac
John Kosh <devprowork@live.com>
2019-09-13 07:39:36 -0400

    Added a .NET Framework Test Project

---
+++ src/zip/ZipUtility/Test/MSNetZipTests/BasicTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSNetZipTests
+{
+  [TestClass]
+  public class BasicTests
+  {
+    [TestMethod]
+    public void CanRun_MSNetTest_True()
+    {
+      Assert.IsTrue(true); /* Sanity check to make sure test framework is working */
+    }
+  }
+}
