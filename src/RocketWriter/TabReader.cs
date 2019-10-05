dace9cc3d8990bcb1eb89292471fc32ddee03ac6
John Kosh <devprowork@live.com>
2019-10-05 15:34:03 -0400

    Added RocketWriter TabReader

---
+++ src/RocketWriter/TabReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace RocketWriter
+{
+  public class TabReader
+  {
+    public const string TAB_HEADER_SIMPLE = "country code	postal code	place name	admin name1	admin code1	admin name2	admin code2	admin name3	admin code3	latitude	longitude	accuracy";
+
+    public const string TAB_LINE_SIMPLE = "US	99591	Saint George Island	Alaska	AK	Aleutians West (CA)	016			56.5944	-169.6186	1";
+
+    /// <summary>
+    /// Splits a string based on tab characters to a ReadOnlySpan of strings.
+    /// TODO: Review actual usefullness of this since there are likely more efficient
+    /// ways to do this byte or stream buffer reading. May apply for header row?
+    /// </summary>
+    /// <returns><![CDATA[ReadOnlySpan<string>]]></returns>
+    public ReadOnlySpan<string> GetTabStrings(string value = TAB_LINE_SIMPLE)
+    {
+      return new ReadOnlySpan<string>(value.Split('\t', StringSplitOptions.None));
+    }
+
+    /// <summary>
+    /// Splits a string based on tab characters to a ReadOnlySpan of JsonEncodedText.
+    /// </summary>
+    /// <returns><![CDATA[ReadOnlySpan<JsonEncodedText>]]></returns>
+    public ReadOnlySpan<JsonEncodedText> GetTabJsonStrings(string value = TAB_LINE_SIMPLE)
+    {
+      return new ReadOnlySpan<JsonEncodedText>(GetTabStrings(value).ToArray().Select(x => JsonEncodedText.Encode(x)).ToArray());
+    }
+  }
+}
---
+++ src/Test/FactoryXUnit/TabReaderSimpleTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using Factory.Util;
+using RocketWriter;
+using System.IO;
+using System.Text.Json;
+
+namespace FactoryXUnit
+{
+  public class TabReaderSimpleTests
+  {
+    public const string JSON_FILE_PATH = @"C:\vscode\github\TestRocket\src\Test\FactoryXUnit\tab-json-strings.json";
+
+    public const string TAB_HEADER_SIMPLE = "country code	postal code	place name	admin name1	admin code1	admin name2	admin code2	admin name3	admin code3	latitude	longitude	accuracy";
+
+    public const string TAB_LINE_SIMPLE = "US	99591	Saint George Island	Alaska	AK	Aleutians West (CA)	016			56.5944	-169.6186	1";
+
+    [Fact]
+    public void GetTabStrings()
+    {
+      /* Arrange */
+      var tr = new TabReader();
+
+      /* Act */
+      var got = tr.GetTabStrings();
+      Console.WriteLine(got.ToArray().ToPrettyString()); // TODO: replace with output.WriteLine logic
+
+      /* Assert */
+      Assert.False(got.IsEmpty);
+    }
+
+
+    [Fact]
+    public void GetTabJsonStrings()
+    {
+      /* Arrange */
+      var tr = new TabReader();
+      var jsonFile = new FileInfo(JSON_FILE_PATH);
+
+      /* Act */
+      var got = tr.GetTabJsonStrings();
+      var gotHeader = tr.GetTabJsonStrings(TAB_HEADER_SIMPLE);
+
+      using var writefs = jsonFile.OpenWrite();
+      using var writer = new Utf8JsonWriter(writefs);
+
+      writer.WriteStartArray();
+      writer.WriteStartObject();
+      for (int i = 0; i < gotHeader.Length; i++)
+      {
+        writer.WriteString(gotHeader[i], got[i]);
+      }
+
+
+      writer.WriteEndObject();
+      writer.WriteEndArray();
+      // await writer.FlushAsync();
+      writer.Dispose();
+      while (writer.BytesPending > 0)
+      {
+        System.Threading.Thread.Sleep(5);
+      }
+      jsonFile.Refresh();
+
+      /* Assert */
+      Assert.False(got.IsEmpty);
+    }
+
+  }
+}
