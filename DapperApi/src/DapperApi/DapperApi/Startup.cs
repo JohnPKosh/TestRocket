82c2e64ea79e591844ece6cedefdb9f3654df868
John Kosh <devprowork@live.com>
2019-10-19 04:04:50 -0400

    added openiddict logic

--- DapperApi/src/DapperApi/DapperApi/Startup.cs
+++ DapperApi/src/DapperApi/DapperApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DapperApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using OpenIddict.Abstractions;
+using OpenIddict.Core;
+using OpenIddict.EntityFrameworkCore.Models;
 
 namespace DapperApi
 {
@@ -25,7 +30,51 @@ namespace DapperApi
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddControllers();
+      services.AddMvc(option => option.EnableEndpointRouting = false);
+
+      // or
+      //services.AddControllersWithViews();
+      //services.AddRazorPages();
+      //services.AddControllers();
+
+      services.AddDbContext<ApplicationDbContext>(options =>
+      {
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+        options.UseOpenIddict();
+      });
+
+      services.AddOpenIddict()
+
+    // Register the OpenIddict core services.
+    .AddCore(options =>
+    {
+                  // Register the Entity Framework stores and models.
+                  options.UseEntityFrameworkCore()
+                         .UseDbContext<ApplicationDbContext>();
+    })
+
+    // Register the OpenIddict server handler.
+    .AddServer(options =>
+    {
+                  // Register the ASP.NET Core MVC binder used by OpenIddict.
+                  // Note: if you don't call this method, you won't be able to
+                  // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
+                  options.UseMvc();
+
+                  // Enable the token endpoint.
+                  options.EnableTokenEndpoint("/connect/token");
+
+                  // Enable the client credentials flow.
+                  options.AllowClientCredentialsFlow();
+
+                  // During development, you can disable the HTTPS requirement.
+                  options.DisableHttpsRequirement();
+
+                })
+
+    // Register the OpenIddict validation handler.
+    .AddValidation();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,14 +87,54 @@ namespace DapperApi
 
       app.UseHttpsRedirection();
 
-      app.UseRouting();
+      //app.UseRouting();
 
-      app.UseAuthorization();
+      //app.UseAuthorization();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
+      //app.UseEndpoints(endpoints =>
+      //{
+      //  endpoints.MapControllers();
+      //});
+
+      app.UseAuthentication();
+
+      app.UseMvcWithDefaultRoute();
+
+      app.UseWelcomePage();
+
+      // Seed the database with the sample application.
+      // Note: in a real world application, this step should be part of a setup script.
+      InitializeAsync(app.ApplicationServices).GetAwaiter().GetResult();
     }
+
+    private async Task InitializeAsync(IServiceProvider services)
+    {
+      // Create a new service scope to ensure the database context is correctly disposed when this methods returns.
+      using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+      {
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await context.Database.EnsureCreatedAsync();
+
+        var manager = scope.ServiceProvider.GetRequiredService<OpenIddictApplicationManager<OpenIddictApplication>>();
+
+        if (await manager.FindByClientIdAsync("console") == null)
+        {
+          var descriptor = new OpenIddictApplicationDescriptor
+          {
+            ClientId = "console",
+            ClientSecret = "388D45FA-B36B-4988-BA59-B187D329C207",
+            DisplayName = "My client application",
+            Permissions =
+                        {
+                            OpenIddictConstants.Permissions.Endpoints.Token,
+                            OpenIddictConstants.Permissions.GrantTypes.ClientCredentials
+                        }
+          };
+
+          await manager.CreateAsync(descriptor);
+        }
+      }
+    }
+
   }
 }
