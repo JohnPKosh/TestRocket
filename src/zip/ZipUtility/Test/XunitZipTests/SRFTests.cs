07a38729836c206e8d37b87bfb9abb3627a1e4f5
John Kosh <devprowork@live.com>
2019-10-26 23:58:25 -0400

    Added SRF tests

---
+++ src/zip/ZipUtility/Test/XunitZipTests/SRFTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+using SRF.Core;
+
+
+namespace XunitZipTests
+{
+    public class SRFTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public SRFTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
+        [Fact]
+        public void CanGetLogDirectoryName_True()
+        {
+            var got = LibraryInfo.LogDirectoryName;
+            output.WriteLine(got);
+            Assert.NotNull(got);
+        }
+
+
+        [Fact]
+        public void CanGetLogDirectoryExists_True()
+        {
+            var got = LibraryInfo.LogDirectoryExists;
+            output.WriteLine(got.ToString());
+            Assert.NotNull(got.ToString());
+        }
+    }
+}
