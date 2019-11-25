1cdbdeb45fdda560f4b58c7baf6f87fe5295c348
John Kosh <devprowork@live.com>
2019-11-25 07:25:39 -0500

    updated SRFTests

--- src/zip/ZipUtility/Test/XunitZipTests/SRFTests.cs
+++ src/zip/ZipUtility/Test/XunitZipTests/SRFTests.cs
@@ -5,35 +5,54 @@ using Xunit;
 using Xunit.Abstractions;
 
 using SRF;
-
+using SRF.IO;
+using System.Reflection;
 
 namespace XunitZipTests
 {
-    public class SRFTests
+  public class SRFTests
+  {
+    private readonly ITestOutputHelper output;
+
+    public SRFTests(ITestOutputHelper output)
     {
-        private readonly ITestOutputHelper output;
-
-        public SRFTests(ITestOutputHelper output)
-        {
-            this.output = output;
-        }
-
-
-        [Fact]
-        public void CanGetLogDirectoryName_True()
-        {
-            var got = IoConfig.AppData.FullName;
-            output.WriteLine(got);
-            Assert.NotNull(got);
-        }
-
-
-        [Fact]
-        public void CanGetLogDirectoryExists_True()
-        {
-            var got = IoConfig.AppData.Exists;
-            output.WriteLine(got.ToString());
-            Assert.NotNull(got.ToString());
-        }
+      this.output = output;
     }
+
+
+    [Fact]
+    public void CanGetLogDirectoryName_True()
+    {
+      var got = IoConfig.AppData.FullName;
+      output.WriteLine(got);
+      Assert.NotNull(got);
+    }
+
+
+    [Fact]
+    public void CanGetLogDirectoryExists_True()
+    {
+      var got = IoConfig.AppData.Exists;
+      output.WriteLine(got.ToString());
+      Assert.NotNull(got.ToString());
+    }
+
+    [Fact]
+    public void CanReadLibraryResource()
+    {
+      var got = ResourceExtensions.ReadLibraryResource(@"res\license.txt");
+      output.WriteLine(got);
+      Assert.NotNull(got);
+    }
+
+    //test-file-01.txt
+
+    [Fact]
+    public void CanReadEmbeddedResource()
+    {
+      var got = ResourceExtensions.ReadEmbeddedResource(@"res\test-file-01.txt", Assembly.GetExecutingAssembly());
+      output.WriteLine(got);
+      Assert.NotNull(got);
+    }
+  }
 }
