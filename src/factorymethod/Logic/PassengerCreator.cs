94f90db6ecbf4a272652b1d1717cee700ee07531
John Kosh <devprowork@live.com>
2020-04-22 07:41:15 -0400

    Exemplified Factory Method Pattern

---
+++ src/factorymethod/Logic/PassengerCreator.cs
@@ -0,0 +1,27 @@
+﻿using factorymethod.Enums;
+using factorymethod.Models;
+using factorymethod.Models.Interfaces;
+
+namespace factorymethod.Logic
+{
+  /// <summary>
+  /// A static creator class that extends how we are using our factory method pattern.
+  /// </summary>
+  public static class PassengerCreator
+  {
+    public static IPassenger Create(PassengerType type)
+    {
+      switch (type)
+      {
+        case PassengerType.Astronaut:
+          return new AstronautFactory().NewPassenger();
+        case PassengerType.Cosmonaut:
+          return new CosmonautFactory().NewPassenger();
+        case PassengerType.Toy:
+          return new ToyFactory().NewPassenger();
+        default:
+          return new AstronautFactory().NewPassenger();
+      }
+    }
+  }
+}
