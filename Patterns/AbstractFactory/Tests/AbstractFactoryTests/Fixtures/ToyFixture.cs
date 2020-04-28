00ef151c6287144d90cf553e2d4e85804c9457cd
John Kosh <devprowork@live.com>
2020-04-28 10:28:44 -0400

    Added initial test plumbing for xunit

---
+++ Patterns/AbstractFactory/Tests/AbstractFactoryTests/Fixtures/ToyFixture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AbstractFactoryLogic.Common;
+
+namespace AbstractFactoryTests.Fixtures
+{
+  public class ToyFixture : IDisposable
+  {
+    public ToyFixture()
+    {
+
+    }
+
+    public readonly string sut_SpeakExpected = FactoryConstants.TOY_SPK;
+
+
+    #region IDisposable Support
+
+    private bool disposedValue = false; // To detect redundant calls
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (!disposedValue)
+      {
+        if (disposing)
+        {
+          // TODO: dispose managed state (managed objects).
+        }
+
+        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+        // TODO: set large fields to null.
+
+        disposedValue = true;
+      }
+    }
+
+    // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+    // ~ToyFixture()
+    // {
+    //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+    //   Dispose(false);
+    // }
+
+    // This code added to correctly implement the disposable pattern.
+    public void Dispose()
+    {
+      // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+      Dispose(true);
+      // TODO: uncomment the following line if the finalizer is overridden above.
+      // GC.SuppressFinalize(this);
+    }
+
+    #endregion
+  }
+}
--- Patterns/AbstractFactory/Tests/AbstractFactoryTests/ToyTests.cs
+++ Patterns/AbstractFactory/Tests/AbstractFactoryTests/ToyTests.cs
@@ -1,13 +1,25 @@
+using AbstractFactoryTests.Fixtures;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace AbstractFactoryTests
 {
-  public class ToyTests
+  public class ToyTests : IClassFixture<ToyFixture>
   {
+    private readonly ITestOutputHelper output;
+    private readonly ToyFixture m_Fix;
+
+    public ToyTests(ITestOutputHelper output, ToyFixture fixture)
+    {
+      this.output = output;
+      m_Fix = fixture;
+    }
+
+
     [Fact]
     public void Test1()
     {
-
+      output.WriteLine(m_Fix.sut_SpeakExpected);
     }
   }
 }
