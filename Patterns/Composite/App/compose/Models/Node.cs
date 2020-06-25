83e01c6d1d714bdae28d1a5553775be0a93bd4b9
John Kosh <devprowork@live.com>
2020-06-25 06:49:30 -0400

    Adding Composite Pattern sample wip

---
+++ Patterns/Composite/App/compose/Models/Node.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace compose.Models
+{
+  public class Node<T> : IEnumerable<Node<T>>
+  {
+    public T Value { get; set; } = default;
+
+    public List<Node<T>> In = new List<Node<T>>(), Out = new List<Node<T>>();
+
+    public IEnumerator<Node<T>> GetEnumerator()
+    {
+      yield return this;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+  }
+
+  public class ChildNodes<T> : Collection<Node<T>>
+  {
+    public ChildNodes(int count)
+    {
+      while (count-- > 0)
+        Add(new Node<T>());
+    }
+
+    public ChildNodes(IEnumerable<T> values)
+    {
+      foreach (var v in values)
+      {
+        Add(new Node<T>() { Value = v });
+      }
+    }
+  }
+
+  public static class NodeExtensions
+  {
+    public static void ConnectTo<T>(this IEnumerable<Node<T>> self, IEnumerable<Node<T>> other)
+    {
+      if (ReferenceEquals(self, other)) return;
+
+      foreach (var from in self)
+        foreach (var to in other)
+        {
+          from.Out.Add(to);
+          to.In.Add(from);
+        }
+    }
+  }
+
+}
---
+++ Patterns/Composite/App/compose/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using compose.Models;
+
+namespace compose
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var dad = new Node<string>
+      {
+        Value = "John"
+      };
+
+      var mom = new Node<string>
+      {
+        Value = "Wendy"
+      };
+
+      dad.ConnectTo(mom);
+
+      //var children = new NodeLayer<string>(2);
+      //mom.ConnectTo(children);
+
+      var childNames = new List<string>() { "Parker", "Sierra" };
+      var children = new ChildNodes<string>(childNames);
+      mom.ConnectTo(children);
+
+      Console.WriteLine("In: {0} / Out {1}", mom.In.Count, mom.Out.Count);
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
