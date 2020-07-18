5f48dcc432f270df95da2c78cf1d9736f40cba66
John Kosh <devprowork@live.com>
2020-07-18 16:07:57 -0400

    Refactored State pattern sample

---
+++ Patterns/State/App/alterstate/Models/PausedState.cs
@@ -0,0 +1,11 @@
+﻿namespace alterstate.Models
+{
+  /// <summary>
+  /// The concrete paused state that inherits from ProcessState.
+  /// </summary>
+  public class PausedState : ProcessState
+  {
+    public PausedState(Context context) : base(context) { }
+  }
+
+}
--- Patterns/State/App/alterstate/Program.cs
+++ Patterns/State/App/alterstate/Program.cs
@@ -11,34 +11,52 @@ namespace alterstate
       con("Countdown has started!");
       hr();
 
-      con("Launch sequence: ");
-      con("Stopped > Paused > Started > Started > Paused > Paused > Stopped");
+      con("Expected Launch sequence: ");
+      con("StoppedState > PausedState > StartedState > StartedState > PausedState > PausedState > StoppedState");
       hr();
 
       // The count down is initially stopped
-      var countDown = new ProcessContext();
+      var countDown = new Context();
+      hr();
+      con("The countdown is initially in the [{0}]!", countDown.CurrentState.GetType().Name);
+      hr();
 
-      //Console.WriteLine(countDown.CurrentState.GetType().Name);
-      //countDown.CurrentState = new Started(countDown);
-      //Console.WriteLine(countDown.CurrentState.GetType().Name);
-      //countDown.CurrentState = new Stopped(countDown);
-      //Console.WriteLine(countDown.CurrentState.GetType().Name);
+      /*
+        Since it makes more sense we added event handling for our base Context class.
+        This is really not a formal part of the state design pattern but it goes a
+        long way to add simple extensible funtionality. The state changes below will
+        write to the console as defined in our logic on each event.
+      */
 
       // The count down is already in stopped state
       countDown.Stop();
+      hr();
+
       // The count down is already in stopped state, pausing will do nothing
       countDown.Pause();
+      hr();
+
       //Making the count down start
       countDown.Start();
+      hr();
+
       // The count down is already started, starting again will do nothing
       countDown.Start();
+      hr();
+
       // Putting the count down in paused state
       countDown.Pause();
+      hr();
+
       // The count down is already paused, pausing will do nothing
       countDown.Pause();
+      hr();
+
       // Stopping the count down
       countDown.Stop();
+      hr();
 
+      con("Failure to Launch!");
     }
 
 
