feed38bb37d8a8c01f550e8bcf49306c3908b5bd
John Kosh <devprohome@live.com>
2020-08-23 17:06:24 -0400

    update open api documentation samples

--- ZipUtility/LogApi/Controllers/WeatherForecastController.cs
+++ ZipUtility/LogApi/Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using LogApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NSwag.Annotations;
 
 namespace LogApi.Controllers
 {
@@ -23,7 +26,13 @@ namespace LogApi.Controllers
       m_Logger = logger;
     }
 
+    /// <summary>Gets a weather forecast</summary>
+    /// <response code="200" nullable="true">Weather Forecast Provided.</response>
+    /// <response code="500" nullable="true">Internal Server Error.</response>
     [HttpGet]
+    [OpenApiOperation("Get", "Gets weather forcasts", "Used to get a collection of weather forcasts")]
+    [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), 200)]
+    [ProducesResponseType(typeof(void), 500)]
     public IEnumerable<WeatherForecast> Get()
     {
       m_Logger.LogInformation("LogInformation {name} {method} Method Called!", nameof(WeatherForecastController), nameof(Get));
@@ -36,6 +45,48 @@ namespace LogApi.Controllers
         Summary = Summaries[rng.Next(Summaries.Length)]
       })
       .ToArray();
+    }
+
+    /// <summary>
+    /// Find a Forecast
+    /// </summary>
+    /// <returns>WeatherForecast</returns>
+    /// <response code="200" nullable="true">Weather Forecast Provided.</response>
+    /// <response code="400" nullable="true">Bad Request.</response>
+    /// <response code="404" nullable="true">Not Found.</response>
+    [HttpGet("{summary}")]
+    [OpenApiOperation("Find Summary", "Gets weather forcast by summary", "Used to get a weather forcast by summary value 0-9")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(WeatherForecast), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(SerializableError), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<WeatherForecast>> FindForecast(int summary)
+    {
+      try
+      {
+        if (!ModelState.IsValid)
+        {
+          return BadRequest(ModelState);
+        }
+
+        await Task.Delay(0);
+        if (summary == 0)
+        {
+          return NotFound();
+        }
+
+        if (summary < 0 || summary > 9) summary = 0;
+        return Ok(new WeatherForecast
+        {
+          Date = DateTime.Now,
+          TemperatureC = 25,
+          Summary = Summaries[summary]
+        });
+      }
+      catch (Exception)
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError);
+      }
     }
   }
 }
