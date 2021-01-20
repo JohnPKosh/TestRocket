a79cca76c3d68367b1330a8f7cc70273c84c63d8
John Kosh <devprohome@live.com>
2021-01-20 09:31:40 -0500

    Add WinArchiveEditor proj

---
+++ Patterns/Integration/App/WinArchiveEditor/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace WinArchiveEditor
+{
+  partial class MainForm
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btn_Generate = new System.Windows.Forms.Button();
+      this.txt_Quantity = new System.Windows.Forms.TextBox();
+      this.lbl_Quantity = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // btn_Generate
+      // 
+      this.btn_Generate.Location = new System.Drawing.Point(676, 404);
+      this.btn_Generate.Name = "btn_Generate";
+      this.btn_Generate.Size = new System.Drawing.Size(112, 34);
+      this.btn_Generate.TabIndex = 0;
+      this.btn_Generate.Text = "Generate";
+      this.btn_Generate.UseVisualStyleBackColor = true;
+      this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
+      // 
+      // txt_Quantity
+      // 
+      this.txt_Quantity.Location = new System.Drawing.Point(12, 37);
+      this.txt_Quantity.Name = "txt_Quantity";
+      this.txt_Quantity.Size = new System.Drawing.Size(150, 31);
+      this.txt_Quantity.TabIndex = 1;
+      this.txt_Quantity.Text = "100";
+      // 
+      // lbl_Quantity
+      // 
+      this.lbl_Quantity.AutoSize = true;
+      this.lbl_Quantity.Location = new System.Drawing.Point(12, 9);
+      this.lbl_Quantity.Name = "lbl_Quantity";
+      this.lbl_Quantity.Size = new System.Drawing.Size(180, 25);
+      this.lbl_Quantity.TabIndex = 2;
+      this.lbl_Quantity.Text = "Quantity to generate:";
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.lbl_Quantity);
+      this.Controls.Add(this.txt_Quantity);
+      this.Controls.Add(this.btn_Generate);
+      this.Name = "MainForm";
+      this.Text = "Win Archive Editor";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btn_Generate;
+    private System.Windows.Forms.TextBox txt_Quantity;
+    private System.Windows.Forms.Label lbl_Quantity;
+  }
+}
+
---
+++ Patterns/Integration/App/WinArchiveEditor/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinArchiveEditor
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void btn_Generate_Click(object sender, EventArgs e)
+    {
+      MessageBox.Show("Generating!");
+    }
+  }
+}
