19f4c15a6c3f46abb1f2771efd381b5310acbeca
John Kosh <devprohome@live.com>
2021-01-24 12:13:39 -0500

    add tab dashboard wip

---
+++ Patterns/Integration/App/WpfArchiveEditor/Forms/TabDashboard.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfArchiveEditor.Forms
+{
+  /// <summary>
+  /// Interaction logic for TabDashboard.xaml
+  /// </summary>
+  public partial class TabDashboard : MetroWindow
+  {
+    public TabDashboard()
+    {
+      InitializeComponent();
+    }
+  }
+}
+
+
+// Bootstrapper link for webview2  https://go.microsoft.com/fwlink/p/?LinkId=2124703
