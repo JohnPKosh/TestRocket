0fd76fd4fa1ac970b9c47ccb2145960631039816
John Kosh <devprohome@live.com>
2021-04-06 17:58:06 -0400

    broke

---
+++ DapperApi/MongoIdent/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace MongoIdent.Models
+{
+	[CollectionName("users")]
+	public class ApplicationUser : MongoIdentityUser<Guid>
+	{
+		public ApplicationUser() : base()
+		{
+		}
+
+		public ApplicationUser(string userName, string email) : base(userName, email)
+		{
+		}
+	}
+
+	[CollectionName("roles")]
+	public class ApplicationRole : MongoIdentityRole<Guid>
+	{
+		public ApplicationRole() : base()
+		{
+		}
+
+		public ApplicationRole(string roleName) : base(roleName)
+		{
+		}
+	}
+}
