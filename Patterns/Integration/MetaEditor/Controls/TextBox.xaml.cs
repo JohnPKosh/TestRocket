24a49adc4f4db76b26154a6df367f918873e1dfc
JohnPKosh <devprohome@live.com>
2022-01-29 10:55:54 -0500

    exploring user controls

---
+++ Patterns/Integration/MetaEditor/Controls/TextBox.xaml.cs
@@ -0,0 +1,90 @@
+﻿using ControlzEx.Theming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetaEditor.Controls
+{
+  /// <summary>
+  /// Interaction logic for uiTextBox.xaml
+  /// </summary>
+  public partial class TextBox : UserControl
+  {
+    public TextBox()
+    {
+      InitializeComponent();
+      this.DataContext = this;
+    }
+
+    private const int DEFAULT_MAX_LEN = 20;
+    private const double DEFAULT_TEXT_FONT_SIZE = 12D;
+
+    #region TextBox Properties
+
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(TextBox), new FrameworkPropertyMetadata(string.Empty));
+
+    public string Text
+    {
+      get { return (string)GetValue(TextProperty); }
+      set { SetValue(TextProperty, value); }
+    }
+
+    public int TextWidth { get; set; } = (int)(DEFAULT_MAX_LEN * DEFAULT_TEXT_FONT_SIZE * 1.0);
+
+    public int TextMinHeight { get; set; } = 26;
+
+    public Thickness TextMargin { get; set; } = new Thickness(4);
+
+    public VerticalAlignment TextVerticalAlignment { get; set; } = VerticalAlignment.Top;
+
+    public HorizontalAlignment TextHorizontalAlignment { get;set; } = HorizontalAlignment.Left;
+
+    public Brush? TextBackground { get; set; } = (SolidColorBrush)Application.Current.Resources["MahApps.Brushes.ThemeBackground"]; // null; {x:Null}
+
+    public Brush TextForeground { get; set; } = (SolidColorBrush)Application.Current.Resources["MahApps.Brushes.ThemeForeground"]; //new SolidColorBrush((Color)Application.Current.Resources["MahApps.Colors.Accent"]);
+
+    public Brush TextBorderBrush { get; set; } = (SolidColorBrush)Application.Current.Resources["MahApps.Brushes.TextBox.Border"];
+
+    public Brush InvalidBorderBrush { get; set; } = new SolidColorBrush((Color)Application.Current.Resources["MahApps.Colors.Accent"]);
+
+    public FontWeight TextFontWeight { get; set; } = FontWeights.Normal;
+
+    public FontStyle TextFontStyle { get; set; } = FontStyles.Normal;
+
+    public double TextFontSize { get; set; } = DEFAULT_TEXT_FONT_SIZE;
+
+    public CharacterCasing TextCasing { get; set; } = CharacterCasing.Normal;
+
+    public FontFamily TextFontFamily { get; set; } = (FontFamily)Application.Current.Resources["MahApps.Fonts.Family.Control"];
+
+    public int MaxLength { get; set; } = DEFAULT_MAX_LEN;
+
+    public int MinLength { get; set; } = 0;
+
+    public int MinLines { get; set; } = 1;
+
+    public int MaxLines { get; set; } = 1;
+
+    //public int MinValue { get; set; }
+
+    //public int MaxValue { get; set; }
+
+
+    #endregion
+
+    public string Title { get; set; }
+
+    public FontWeight TitleFontWeight { get; set; }
+  }
+}
---
+++ Patterns/Integration/MetaEditor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MahApps.Metro.Controls;
+using MetaEditor.Util;
+using Microsoft.Web.WebView2.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetaEditor
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void webView_Loaded(object sender, RoutedEventArgs e)
+    {
+      var ctrl = (WebView2)sender;
+      ctrl.SetAppContextRelativeSource("elm/html/test.html");
+    }
+  }
+}
