37aaeb37964ff4d2da07aede6dc23a80b64f61d3
JohnPKosh <devprohome@live.com>
2022-02-02 10:06:13 -0500

    add MinValue validation

--- Patterns/Integration/MetaEditor/Controls/TextBox.Validators.cs
+++ Patterns/Integration/MetaEditor/Controls/TextBox.Validators.cs
@@ -13,7 +13,7 @@ namespace MetaEditor.Controls
     {
       ValidState = new ValidationState().Validate(new[] {
         CheckRequired,
-        CheckWaldo
+        CheckMin
       });
 
       if (!ValidState.IsValid)
@@ -40,6 +40,45 @@ namespace MetaEditor.Controls
     private ValidationState CheckWaldo(ValidationState existing)
     {
       if (IsRequired && !txtValue.Text.Contains("Waldo", StringComparison.InvariantCultureIgnoreCase)) existing.FailureMessage = "*The field does not contain waldo!";
+      return existing;
+    }
+
+
+    private ValidationState CheckMin(ValidationState existing)
+    {
+      var minStr = MinValue?.ToString();
+      if (string.IsNullOrWhiteSpace(minStr)) return existing;
+      minStr = minStr.Trim();
+
+      var val = txtValue?.Text.Replace("$", String.Empty).Replace(",", String.Empty);
+      if (string.IsNullOrWhiteSpace(val))
+      {
+        existing.FailureMessage = $"*The value must be equal to or greater than {minStr} date!";
+        return existing;
+      }
+      val = val.Trim();
+
+      if (minStr.Contains('/') || (minStr.Contains('-') && !minStr.StartsWith("-"))) // check for dates
+      {
+        if(DateTime.TryParse(minStr, out DateTime minDt))
+        {
+          if(!string.IsNullOrWhiteSpace(val) && DateTime.TryParse(val, out DateTime valueDt))
+          {
+            if(valueDt >= minDt) return existing;
+          }
+          existing.FailureMessage = $"*The value must be equal to or greater than {minStr} date!";
+          return existing;
+        }
+      }
+      else if(!string.IsNullOrWhiteSpace(val))
+      {
+        val = val.Replace("$", String.Empty).Replace(",", String.Empty);
+        if(decimal.TryParse(minStr, out decimal minDecimal) && decimal.TryParse(val, out decimal valueDecimal))
+        {
+          if (valueDecimal >= minDecimal) return existing;
+        }        
+      }
+      existing.FailureMessage = $"*The value must be equal to or greater than {minStr}!";
       return existing;
     }
   }
