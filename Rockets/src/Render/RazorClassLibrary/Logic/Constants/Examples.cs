acaa2b5e5ab9e5b5a7b16730faed81dbb062e90e
John Kosh <john.kosh@greatdayimprovements.com>
2023-11-16 07:07:51 -0500

    Add .net 8 HtmlRenderer component rendering sample

---
+++ Rockets/src/Render/RazorClassLibrary/Logic/Constants/Examples.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Logic.Constants;
+
+public static class HTML
+{
+  public const string START_TAG_PREFIX = "<";
+  public const string START_TAG_SUFFIX = ">";
+
+  public const string END_TAG_PREFIX = "</";
+  public const string END_TAG_SUFFIX = ">";
+}
+
+public static class CSHARP
+{
+  public const string BODY_PREFIX = "{";
+  public const string BODY_SUFFFIX = "}";
+
+  public const string ACCESS_PUBLIC = "public";
+  public const string ACCESS_PROTECTED = "protected";
+  public const string ACCESS_INTERNAL = "internal";
+  public const string ACCESS_PROTECTED_INTERNAL = "protected internal";
+  public const string ACCESS_PRIVATE = "private";
+  public const string ACCESS_PRIVATE_PROTECTED = "private protected";
+  public const string ACCESS_FILE = "file";
+
+  public const string PARTIAL_MODIFIER = "partial";
+
+  public const string MODIFIER_ABSTRACT = "abstract";
+  public const string MODIFIER_VIRTUAL = "virtual";
+  public const string MODIFIER_STATIC = "static";
+
+  public const string ATTRIBUTE_PREFIX = ":";
+  public const string ATTRIBUTE_SEPERATOR = ",";
+
+  public const string TYPE_PARAMETER_PREFIX = "<";
+  public const string TYPE_PARAMETER_SEPERATOR = ",";
+  public const string TYPE_PARAMETER_SUFFIX = ">";
+
+  public const string TYPE_CONSTRAINT_PREFIX = "where";
+  public const string TYPE_CONSTRAINT_SEPERATOR = ":";
+}
+
+
+/*
+
+public: Access isn't restricted.
+protected: Access is limited to the containing class or types derived from the containing class.
+internal: Access is limited to the current assembly.
+protected internal: Access is limited to the current assembly or types derived from the containing class.
+private: Access is limited to the containing type.
+private protected: Access is limited to the containing class or types derived from the containing class within the current assembly.
+file: The declared type is only visible in the current source file. File scoped types are generally used for source generators.
+
+*/
---
+++ Rockets/src/Render/RazorClassLibrary/Logic/MarkupTag.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+using Microsoft.AspNetCore.Components;
+
+namespace RazorClassLibrary.Logic;
+
+public struct MarkupTag
+{
+  public MarkupTag(string name, string content, bool isHtmlEncoded = false, IDictionary<string, string>? parameters = null)
+  {
+    Name = name;
+    Content = [content];
+    IsHtmlEncoded = isHtmlEncoded;
+    Parameters = parameters;
+  }
+
+  public MarkupTag(string name, object[] content, bool isHtmlEncoded = false, IDictionary<string, string>? parameters = null)
+  {
+    Name = name;
+    Content = content;
+    IsHtmlEncoded = isHtmlEncoded;
+    Parameters = parameters;
+  }
+
+  public string Name { get; set; }
+
+  public object[] Content { get; set; }
+
+  public bool IsHtmlEncoded { get; set; }
+
+  public IDictionary<string, string>? Parameters { get; set; }
+
+  public override readonly string ToString()
+    => HtmlTagGenerator.GetTag(Name, GetContentString(Content), false, Parameters);
+
+  /// <summary>
+  /// Casts a <see cref="MarkupTag"/> to a <see cref="MarkupString"/>.
+  /// </summary>
+  /// <param name="value">The <see cref="string"/> value.</param>
+  public static explicit operator MarkupString(MarkupTag value)
+      => new(value.ToString());
+
+
+  private static string? GetContentString(object[] objects)
+  {
+    if (objects == null) return null;
+    var sb = new StringBuilder();
+    foreach (var c in objects)
+    {
+      sb.Append(c.ToString());
+    }
+    return sb.ToString();
+  }
+
+}
